baaf08ee576c66253638115a9fdc24521f2d056f
francisco <francisco@franciscos-Mac-Pro.local>
2023-10-21 22:50:28 -0500

    dashboard

--- client/js/global.js
+++ client/js/global.js
@@ -1,8 +1,17 @@
 const footer  = `
-    <h1>footer</h1> 
+    <secction class="d-flex flex-column justify-content-center align-items-center p-3">
+        <small>Distribuido por: Francisco Muñeton - Ferney de Jesus Jaramillo</small>
+        <small>2023 - sena.edu.co</small>
+    </section>
+`;
+
+const submenu = `
+    submenu
 `;
 
 $(()=>{
     let blockFooter = document.querySelector('#footer');
+    let blockSubmenu = document.querySelector('#subheader');
     $(blockFooter).append(footer);
+    $(blockSubmenu).append(submenu);
 })
--- client/js/menu.js
+++ client/js/menu.js
@@ -1,5 +1,34 @@
 let menuPrincipal =`
-    <h1>Hello menu</h1>
+   <div class="headMenu">
+   <div class="logo mx-auto pt-3 pb-3">
+    <img src="https://famu.com.co/logo2.png" class="img-fluid" alt="">
+    </div>
+   </div>  
+   <div class="boxMenu">
+    <ul>
+        <li>
+            <a class="active"><i class="fa-solid fa-chart-simple"></i> <span>Dasboard</span> </a>
+        </li> 
+        <li>
+             <a><i class="fa-solid fa-calendar-check"></i> <span>Agendar Citas</span> </a>
+        </li> 
+        <li>
+            <a><i class="fa-solid fa-users"></i> <span>Pacientes</span> </a>
+        </li>
+        <li>
+            <a><i class="fa-solid fa-user-doctor"></i> <span>Medicos</span> </a>
+        </li>
+        <li>
+            <a><i class="fa-solid fa-person"></i> <span>Empleados</span> </a>
+        </li>
+        <li>
+            <a><i class="fa-solid fa-bed"></i> <span>Consultorios</span> </a>
+        </li>
+        <li>
+            <a><i class="fa-solid fa-barcode"></i> <span>Medicamentos</span> </a>
+        </li>
+    </ul>
+   </div>    
 `;
  
 
